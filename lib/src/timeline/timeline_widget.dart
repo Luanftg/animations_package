1b6fc466ed780ecbd8c2fa1967d6e49e15c0034e
Luanftg <luanftgimenez@gmail.com>
2024-03-24 21:07:13 -0300

    [refact]: timeline widget

--- lib/src/timeline/timeline_widget.dart
+++ lib/src/timeline/timeline_widget.dart
@@ -1,23 +1,24 @@
 import 'package:animations_package/animations_package.dart';
-import 'package:animations_package/src/timeline/timeline_config.dart';
+import 'package:animations_package/src/internal/lib/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class TimeLineWidget extends StatefulWidget {
-  const TimeLineWidget({
-    super.key,
-    required this.dataCard,
-    required this.startDate,
-    required this.endDate,
-    this.timeLineConfig,
-    this.onHover,
-  });
+  const TimeLineWidget(
+      {super.key,
+      required this.dataCard,
+      required this.startDate,
+      required this.endDate,
+      this.timeLineConfig,
+      this.onHover,
+      this.widget});
 
   final DataCard dataCard;
   final DateTime startDate;
   final DateTime endDate;
   final TimeLineConfig? timeLineConfig;
   final Function(PointerHoverEvent event)? onHover;
+  final Widget? widget;
 
   @override
   State<TimeLineWidget> createState() => _TimeLineWidgetState();
@@ -56,10 +57,15 @@ class _TimeLineWidgetState extends State<TimeLineWidget> {
 
   _handleDragUpdate(DragUpdateDetails details) {
     final dd = details.primaryDelta ?? 1;
-    setState(() {
-      startDate = startDate.add(Duration(days: -dd.toInt()));
-      endDate = endDate.add(Duration(days: -dd.toInt()));
-    });
+    final addDays = startDate.add(Duration(days: -dd.toInt()));
+    final endDays = endDate.add(Duration(days: -dd.toInt()));
+    if (addDays.isBefore(dataCard.endDate) &&
+        endDays.isAfter(dataCard.startDate)) {
+      setState(() {
+        startDate = startDate.add(Duration(days: -dd.toInt()));
+        endDate = endDate.add(Duration(days: -dd.toInt()));
+      });
+    }
   }
 
   void handleOnTapUp(TapUpDetails details) {
@@ -71,7 +77,7 @@ class _TimeLineWidgetState extends State<TimeLineWidget> {
           showDialog(
             context: context,
             builder: (context) =>
-                AlertDialog(content: Text('Clicou no ${serie.name}')),
+                AlertDialog(content: widget.widget ?? Text(serie.name)),
           );
         }
       }
