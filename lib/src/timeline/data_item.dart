5ce308ca72eb0b670c38c9654cdc5122cb29feb8
Luanftg <luanftgimenez@gmail.com>
2024-03-25 21:24:13 -0300

    [refact]: add object to DataItem

--- lib/src/timeline/data_item.dart
+++ lib/src/timeline/data_item.dart
@@ -2,9 +2,11 @@ class DataItem {
   DateTime timestamp;
   double? value;
   String? name;
+  Object? object;
   DataItem({
     required this.timestamp,
     this.value,
     this.name,
+    this.object,
   });
 }
